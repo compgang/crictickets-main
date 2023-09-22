37c83972ba192f23730d79cccdb6727e39fac792
imanoobhehe <vvashvath08@gmail.com>
2023-09-22 17:58:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 # Made with love by Arnav, Khushi, Shreya and Ashvath (we need the marks)
-# CricTickets™ is a trademarked name(jk) by Arnav & Co.
+# CricTickets™ is a trademarked name (jk) by Arnav & Co.
