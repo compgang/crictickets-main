0da4c57f9229c58355eb7312b9e73c634ad37a05
imanoobhehe <vvashvath08@gmail.com>
2023-09-23 18:32:05 +0530

    Bug fixing
    Fixed up login loop case, added cases for accounts that already exist, fixed other looping issues.

--- main.py
+++ main.py
@@ -8,21 +8,22 @@ time.sleep(1)
 print("Welcome to CricTickets, the best place to get your IPL tickets!")
 time.sleep(3)
 
+
 # Starting with the log in system
 x = 0
 a = 0
 con = sql.connect("data.db")
 cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS users (email, username, password)")
-while x == 0 or x != 1 or x != 2:
+while x != 1 and x != 2:
     a = 0
     x = int(input('''1. Create a new account
 2. Log into your account
 What is  your choice?: '''))
     while a == 0:
         if x == 2:
-            username = input("Enter your username!: ")
-            password = input("Enter your password!: ")
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
 
             statement = f"SELECT username from users WHERE username='{username}' AND Password = '{password}';"
             cur.execute(statement)
@@ -32,23 +33,32 @@ What is  your choice?: '''))
             else:
                 print("You have successfully logged in! Welcome", username, "!")
                 a = 1
-                x = 0
+                x = 1
                 break
         elif x == 1:
             email = input("Enter your E-mail: ")
             username = input("Enter your username: ")
             password = input("Enter your password: ")
             password2 = input("Enter your password again: ")
+
             while password != password2:
                 print("Both the passwords entered do not match! Try again.")
                 email = input("Enter your E-mail: ")
                 username = input("Enter your username: ")
                 password = input("Enter your password: ")
                 password2 = input("Enter your password again: ")
-            cur.execute("insert into users (email, username, password) values (?, ?, ?)",
-                        (email, username, password))
-            x = 0
-            a = 1
-            continue
+            statement = f"SELECT email FROM users WHERE email = '{username}'"
+            cur.execute(statement)
+            if cur.fetchone:
+                cur.execute("insert into users (email, username, password) values (?, ?, ?)",
+                            (email, username, password))
+                con.commit()
+                x = 0
+                a = 1
+                continue
+            else:
+                print("A user is already registered to this E-mail! Retry.")
+
 
 # Log in system finish
+
