56cedc755057756d4bf8b453642ef38d24deade5
imanoobhehe <145670785+imanoobhehe@users.noreply.github.com>
2023-10-16 11:57:09 +0530

    Restructured some areas

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # Made with love by Arnav, Khushi, Shreya and Ashvath (we need the marks)
 # CricTickets™ is a trademarked name by Arnav & Co. (jk)
 
+# Import corner
 import sqlite3 as sql   # Dependency 1
 import time
 import pwinput as mask
+import re
+
+
 time.sleep(1)
 print("Welcome to CricTickets, the best place to get your IPL tickets!")
 time.sleep(3)
@@ -35,27 +39,31 @@ What is  your choice?: '''))
                 x = 1
         elif x == 1:
             email = input("Enter your E-mail: ")
-            username = input("Enter your username: ")
-            password = mask.pwinput(prompt="Enter your password: ", mask='*')
-            password2 = mask.pwinput(prompt="Enter your password again: ", mask='*')
+            email_valid = bool(re.match(r"[^@]+@[^@]+\.[^@]+", email))
 
-            while password != password2:
-                print("Both the passwords entered do not match! Try again.")
-                email = input("Enter your E-mail: ")
+            if email_valid:
                 username = input("Enter your username: ")
                 password = mask.pwinput(prompt="Enter your password: ", mask='*')
                 password2 = mask.pwinput(prompt="Enter your password again: ", mask='*')
-            statement = f"SELECT email FROM users WHERE email = ? and username = ?"
-            cur.execute(statement, (email, username))
-            if not cur.fetchone():
-                cur.execute("insert into users (email, username, password) values (?, ?, ?)",
-                            (email, username, password))
-                con.commit()
-                x = 0
-                a = 1
-            else:
-                print("A user is already registered to this E-mail! Retry.")
+                while password != password2:
+                    print("Both passwords do not match! Try again.")
+                    username = input("Enter your username: ")
+                    password = mask.pwinput(prompt="Enter your password: ", mask='*')
+                    password2 = mask.pwinput(prompt="Enter your password again: ", mask='*')
 
+                statement = f"SELECT email FROM users WHERE email = ? and username = ?"
+                cur.execute(statement, (email, username))
+                if not cur.fetchone():
+                    cur.execute("insert into users (email, username, password) values (?, ?, ?)",
+                            (email, username, password))
+                    con.commit()
+                    print("Account has been created! Please log in to your account now.")
+                    x = 0
+                    a = 1
+                else:
+                    print("A user is already registered to this E-mail! Retry.")
+            else:
+                print("This is an invalid email! Retry.")
 time.sleep(10)
 
 # Log in system finished
