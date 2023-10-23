5190db3958c2ddf3d2888a224c2ef7aa4ab36f82
imanoobhehe <145670785+imanoobhehe@users.noreply.github.com>
2023-10-23 20:02:16 +0530

    New files

--- add_match_sql.py
+++ add_match_sql.py
@@ -15,7 +15,7 @@ while a == True:
         cur.execute(f"select matchID from matches where Team1 = '{team1}' and Team2 = '{team2}'")
         if not cur.fetchone():
             location1 = input("Enter location: ")
-            date = input("Enter date: ")
+            date = input("Enter date (format: yyyy-mm-dd): ")
             stadium = input("Enter stadium: ")
             desc = input("Enter match description: ")
             price_range = input("Enter price range: ")
@@ -25,15 +25,21 @@ while a == True:
             while cur.fetchone():
                 matchID = ''.join(ran.choices(string.digits, k=6))
             cur.execute('''insert into matches (matchID, Team1, Team2, location, date, stadium, desc, price_range,
-    time1a) values (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
+    time1) values (?, ?, ?, ?, ?, ?, ?, ?, ?)''',
                         (matchID, team1, team2, location1, date, stadium, desc, price_range, time1))
 
             con.commit()
             print("Match has been created.")
         else:
             print("Match for these two teams already exists.")
+            a = False
     elif x == 2:
         matchID = int(input("Enter match ID from database: "))
-        cur.execute(f"delete from matches where matchID = '{matchID}'")
-        con.commit()
-        print("Match for", matchID, "has been deleted.")
+        cur.execute(f"select matchID from matches where matchID = {matchID}'")
+        if not cur.fetchone():
+            cur.execute(f"delete from matches where matchID = '{matchID}'")
+            con.commit()
+        else:
+            print("Match for", matchID, "has been deleted.")
+            a = False
+
--- clear_databse.py
+++ clear_databse.py
@@ -3,22 +3,22 @@ import time
 print("Processing....")
 con = sql.connect("data.db")
 cur = con.cursor()
-statement = f"drop table users"
+cur.execute(f"drop table users")
 
-cur.execute(statement)
-statement = f'''create table users 
+
+cur.execute(f'''create table users 
 (
 userID char primary key,
 email char, 
 username char,
 password char
 )
-'''
-cur.execute(statement)
-statement = f"drop table orders"
-cur.execute(statement)
+''')
 
-statement = (f'''create table orders
+cur.execute(f"drop table orders")
+
+
+cur.execute(f'''create table orders
 (
 userID char primary key, 
 email char,
@@ -28,21 +28,8 @@ food_n_drinks bool,
 transport bool
 )
 ''')
-cur.execute(statement)
-cur.execute("drop table matches")
-cur.execute(f'''create table matches
-(
-matchID int,
-Team1 char,
-Team2 char, 
-location char, 
-date date,
-stadium char,
-desc char, 
-price_range char, 
-time1 time
-)
-''')
+
+
 
 time.sleep(2)
 print("data.db has been cleared. have a great day!")
