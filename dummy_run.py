34a354d291082d1df11c63ac3d7b34b39974038b
imanoobhehe <vvashvath08@gmail.com>
2024-03-01 17:37:29 +0530

    2024 Server Overhaul

--- dummy_run.py
+++ dummy_run.py
@@ -1,18 +1,9 @@
-import sqlite3
-import string
-import random as ran
+import mysql.connector as mysql
+con = mysql.connect(
+host  = "localhost",
+username = "root",
+password = "mysql",
+)
+cur = con.cursor()
 
-# Connect to the database
-conn = sqlite3.connect('data.db')
-
-# Create a cursor object
-cur = conn.cursor()
-
-
-
-userid1 = ''.join(ran.choices(string.ascii_uppercase, k=4))
-userid2 = ''.join(ran.choices(string.digits, k=10))
-userid3 = str(userid1) + str(userid2)
-print (userid3)
-cur.execute(f"select orderid from matches where orderID = '{userid3}'")
-
+cur.execute("drop database ('cricktickets-db-exported',)")
